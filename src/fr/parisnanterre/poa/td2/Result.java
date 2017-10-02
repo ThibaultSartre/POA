027b50c62128252f2fe80dd601458d934ab7e9c7
Thibault <sartre.thibault@hotmail.fr>
2017-10-02 17:34:19 +0200

    UpdateTD2

--- src/fr/parisnanterre/poa/td2/Result.java
+++ src/fr/parisnanterre/poa/td2/Result.java
@@ -4,15 +4,6 @@ package fr.parisnanterre.poa.td2;
  * Created by thsartre on 02/10/2017.
  */
 public enum Result {
-    WIN("win"),LOST("lost"),TIE("tie");
+    WIN,LOST,TIE;
 
-    private String result;
-
-    private Result(String result){
-        this.result = result;
-    }
-
-    public String getResult() {
-        return result;
-    }
 }
